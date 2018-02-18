bc6f153e01c35b42c75cce68322e0fc4a7b526b8
Kamil <kuchciakkamil@gmail.com>
2018-02-18 21:07:41 +0100

    add clamp player's position to defined movable zone

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,19 @@ public class PlayerController : MonoBehaviour {
 
 	public float velocity;
 
+	// Defines bottom left and top right positions of movable zone of player's ship
+	private Vector3 bottomLeft;
+	private Vector3 topRight;
+
 	// Use this for initialization
 	void Start () {
+		var cameraDistance = transform.position.z - Camera.main.transform.position.z;
+		bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, cameraDistance));
+		topRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 0.4f, cameraDistance));
+		var halfWidth = gameObject.renderer.bounds.size.x / 2;
+		var halfHeight = gameObject.renderer.bounds.size.y / 2;
+		bottomLeft += new Vector3(halfWidth, halfHeight, 0);
+		topRight -= new Vector3(halfWidth, halfHeight, 0);
 	}
 	
 	// Update is called once per frame
@@ -29,5 +40,9 @@ public class PlayerController : MonoBehaviour {
 	void HandleMove (Vector3 moveDirection) {
 		var moveVector = moveDirection * velocity * Time.deltaTime;
 		transform.position += moveVector;
+		var pos = transform.position;
+		var clampedX = Mathf.Clamp(pos.x, bottomLeft.x, topRight.x);
+		var clampedY = Mathf.Clamp(pos.y, bottomLeft.y, topRight.y);
+		transform.position = new Vector3(clampedX, clampedY, pos.z);
 	}
 }
