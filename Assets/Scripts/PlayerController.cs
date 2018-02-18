51c7c14c2f55d1384c6d0664b73140d4dad7fb2b
Kamil <kuchciakkamil@gmail.com>
2018-02-18 18:49:16 +0100

    add Player to scene and PlayerController script to enable moving of spacecraft

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float velocity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+
+	void Move () {
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			HandleMove(new Vector2 (-1.0f, 0.0f));
+		} else if (Input.GetKey(KeyCode.RightArrow)) {
+			HandleMove(new Vector2 (1.0f, 0.0f));
+		} else if (Input.GetKey(KeyCode.UpArrow)) {
+			HandleMove(new Vector2 (0.0f, 1.0f));
+		} else if (Input.GetKey(KeyCode.DownArrow)) {
+			HandleMove(new Vector2 (0.0f, -1.0f));
+		}
+	}
+
+	void HandleMove (Vector2 moveDirection) {
+		var moveVector = new Vector3 (moveDirection.x * velocity, moveDirection.y * velocity, 0.0f) * Time.deltaTime;
+		transform.position += moveVector;
+	}
+}
