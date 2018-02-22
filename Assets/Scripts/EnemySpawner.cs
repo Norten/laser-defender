fb00d689abd4b327c5ed417807bcb397c4f8a4e8
Kamil <kuchciakkamil@gmail.com>
2018-02-22 21:45:02 +0100

    add position for spawning enemy

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,8 +6,10 @@ public class EnemySpawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		var enemy = Instantiate(enemyPrefab, new Vector3(8, 10, 0), Quaternion.FromToRotation(new Vector3(0, 1, 0), new Vector3(0, -1, 0))) as GameObject;
-		enemy.transform.parent = transform;
+		foreach (Transform child in transform) {
+			var enemy = Instantiate(enemyPrefab, child.position, Quaternion.FromToRotation(new Vector3(0, 1, 0), new Vector3(0, -1, 0))) as GameObject;
+			enemy.transform.parent = child;
+		}
 	}
 	
 	// Update is called once per frame
