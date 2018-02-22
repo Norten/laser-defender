de9569c79238f11da9959796fcd32fb745f47a3c
Kamil <kuchciakkamil@gmail.com>
2018-02-22 21:01:50 +0100

    add enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+	public GameObject enemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+		var enemy = Instantiate(enemyPrefab, new Vector3(8, 10, 0), Quaternion.FromToRotation(new Vector3(0, 1, 0), new Vector3(0, -1, 0))) as GameObject;
+		enemy.transform.parent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
