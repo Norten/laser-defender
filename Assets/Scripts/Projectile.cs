173e8d95c13b9553a14a788e3903ba9586980bc2
Nort <kuchciakkamil@gmail.com>
2018-02-25 18:20:51 +0100

    use rigidbody instead of updating position manually

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,11 +5,11 @@ public class Projectile : MonoBehaviour {
 	public float speed = 10f;
 	// Use this for initialization
 	void Start () {
-	
+		rigidbody2D.velocity = new Vector2(0, speed);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position += Vector3.up * speed * Time.deltaTime;
+
 	}
 }
